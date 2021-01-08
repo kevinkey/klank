4e142c946e2c237aa24584436f5e8d88eab5750c
Schwieterman <aaron.schwieterman@c-it001-52034.gentex.com>
2021-01-08 13:22:08 -0500

    Adding #60

--- dragon.rb
+++ dragon.rb
@@ -54,6 +54,7 @@ module Klank
 
             draw = DRAW[@level] + @game.dungeon.danger() + @game.escalation
 
+            view_bag(true)
             @game.broadcast("The dragon attacks, drawing #{draw} cubes...")
             sleep(1)
 
@@ -84,13 +85,23 @@ module Klank
             @game.broadcast("#{actual} dragon cube(s) were added back to the dragon bag!")
         end
 
-        def view_bag()
+        def view_bag(show_chances = false)
             if @bag.length > 0
                 cubes = []
+                count = @bag.select { |c| c.to_s == i.to_s }.count
                 @game.player.each_with_index do |p, i|
-                    cubes << {"PLAYER" => p.name, "COUNT" => @bag.select { |c| c.to_s == i.to_s }.count}
+                    if show_chances
+                        cubes << {"PLAYER" => p.name, "COUNT" => count, "DRAW CHANCE" => (count.fdiv(@bag.count) * 100).round(2).to_s}
+                    else
+                        cubes << {"PLAYER" => p.name, "COUNT" => count}
+                    end
                 end
-                cubes << {"PLAYER" => "Dragon", "COUNT" => @bag.select { |c| c.to_s == "D" }.count}
+                count = @bag.select { |c| c.to_s == "D" }.count
+                if show_chances
+                    cubes << {"PLAYER" => "Dragon", "COUNT" => count, "DRAW CHANCE" => (count.fdiv(@bag.count) * 100).round(2).to_s}
+                else
+                    cubes << {"PLAYER" => "Dragon", "COUNT" => count}
+                end
                 @game.broadcast("\nDRAGON BAG\n#{Klank.table(cubes)}")
             end
         end
